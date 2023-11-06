601227be48d4c8b8a565f82516333ceca9f19466
ThatBoiDev <mart.zielman@kpnmail.nl>
2023-11-06 22:08:51 +0100

    code convention should be done as well xD

--- src/game.py
+++ src/game.py
@@ -1,87 +1,72 @@
+"""
+The main game file. This contains the room and all assets and processes.
+"""
+
 from DSEngine import *
 from pygame import Vector2
 from pygame.display import update
-from sys import exit
-#from random import randint
+# from random import randint
 
-default_title = "Project: Solitude"
-height = 720
-width = 1280
-sprite_size = {
+TITLE = "Project: Solitude"
+HEIGHT = 720
+WIDTH = 1280
+SPR_SIZE = {
     "width": 24,
     "height": 32
-
 }
 
-
-#moved variables from main to here
-#
-#
-window = Window(title=default_title, fps=60, size=(width, height), bg=(100, 100, 100))
+window = Window(title=TITLE, fps=60, size=(WIDTH, HEIGHT), bg=(100, 100, 100))
 audio_man = AudioManager()
 
 down1 = Image2D(filename="Assets/Player/tile000.png", position=Vector2(150, 55))
 down2 = Image2D(filename="Assets/Player/tile001.png", position=Vector2(150, 55))
 down3 = Image2D(filename="Assets/Player/tile002.png", position=Vector2(150, 55))
-down = Spritesheet(down2,down2,down2,down2,down2,down2,down2,down2,down2,down2,down2,down2,\
-                    down3,down3,down3,down3,down3,down3,down3,down3,down3,down3,down3,down3,)
+down = Spritesheet(*([down2] * 12 + [down3] * 12))
 up1 = Image2D(filename="Assets/Player/tile003.png", position=Vector2(150, 55))
 up2 = Image2D(filename="Assets/Player/tile004.png", position=Vector2(150, 55))
 up3 = Image2D(filename="Assets/Player/tile005.png", position=Vector2(150, 55))
-up = Spritesheet(up2,up2,up2,up2,up2,up2,up2,up2,up2,up2,up2,up2,\
-                    up3,up3,up3,up3,up3,up3,up3,up3,up3,up3,up3,up3,)
+up = Spritesheet(*([up2] * 12 + [up3] * 12))
 left1 = Image2D(filename="Assets/Player/tile006.png", position=Vector2(150, 55))
 left2 = Image2D(filename="Assets/Player/tile007.png", position=Vector2(150, 55))
 left3 = Image2D(filename="Assets/Player/tile008.png", position=Vector2(150, 55))
-left = Spritesheet(left2,left2,left2,left2,left2,left2,left2,left2,left2,left2,left2,left2,\
-                    left3,left3,left3,left3,left3,left3,left3,left3,left3,left3,left3,left3,)
+left = Spritesheet(*([left2] * 12 + [left3] * 12))
 right1 = Image2D(filename="Assets/Player/tile009.png", position=Vector2(150, 55))
 right2 = Image2D(filename="Assets/Player/tile010.png", position=Vector2(150, 55))
 right3 = Image2D(filename="Assets/Player/tile011.png", position=Vector2(150, 55))
-right = Spritesheet(right2,right2,right2,right2,right2,right2,right2,right2,right2,right2,right2,right2,\
-                    right3,right3,right3,right3,right3,right3,right3,right3,right3,right3,right3,right3,)
+right = Spritesheet(*([right2] * 12 + [right3] * 12))
 
 animationsheet = AnimationSheet(default=down1, down=down, up=up, left=left, right=right)
 sprite = AnimatedSprite2D(layer=1, sheet=animationsheet, position=Vector2(150, 55))
 # text = Text2D("Hello World", position=Vector2(550, 335))
-bedsheet=AnimationSheet(default=Image2D("Assets/bed.png"), size=Vector2(64, 1))
+bedsheet = AnimationSheet(default=Image2D("Assets/bed.png"), size=Vector2(64, 1))
 rect0 = AnimatedSprite2D(sheet=bedsheet, layer=1)
 startpos = sprite.position
-#
-#
-#end of variables moved from main()
-rect=Rect2D()
+rect = Rect2D()
 
-scene="scenetest"
-
+SCENE = "scenetest"
 
 def test(keys):
     bedsheet.visible=False
     sprite.visible=False
     rect0.visible=False
-    
+
     if keys[key_to_scancode(" ")]:
-        global scene
-        scene="main"
-        print(scene)
-    
-        
+        global SCENE
+        SCENE ="main"
+        print(SCENE)
 
 def mainroom(keys):
     rect.visible=False
     acc = Vector2(0.0, 0.0)
 
-    sheet_to_play="" #sheet will be played after these if commands 
-    #so it wont switch between the animations every frame that makes animation stop on holding
-    #two buttons
-    
+    sheet_to_play=""
+
     bedsheet.visible=True
     sprite.visible=True
     rect0.visible=True
-    
+
     if keys[key_to_scancode("d")]:
         sprite.sprites.default = right1
-        
         sheet_to_play="right"
     if keys[key_to_scancode("a")]:
         sprite.sprites.default = left1
@@ -92,39 +77,31 @@ def mainroom(keys):
     if keys[key_to_scancode("s")]:
         sprite.sprites.default = down1
         sheet_to_play="down"
-    
-    
-    acc.x = (keys[key_to_scancode("d")]-keys[key_to_scancode("a")])#*window.delta
-    acc.y = (keys[key_to_scancode("s")]-keys[key_to_scancode("w")])#*window.delta
-    
-    if acc==Vector2(0,0): sheet_to_play=""
+
+    acc.x = keys[key_to_scancode("d")] - keys[key_to_scancode("a")] # * window.delta
+    acc.y = keys[key_to_scancode("s")] - keys[key_to_scancode("w")] # * window.delta
+
+    if acc==Vector2(0,0):
+        sheet_to_play=""
+
     if sheet_to_play !="" and (not sprite.playing or sprite.sheet_name != sheet_to_play):
         sprite.play_sheet(sheet_to_play)
     elif sheet_to_play=="":
         sprite.frame=sprite.sheet_length
         sprite.playing=False
     sprite.move(acc)
-    # print(f"Sprite position: {sprite.position}")
 
-    if sprite.position.x < 0:
-        sprite.position.x = 0
-    if sprite.position.x > width - sprite_size["width"]:
-        sprite.position.x = width - sprite_size["width"]
-    if sprite.position.y < 0:
-        sprite.position.y = 0
-    if sprite.position.y > height - sprite_size["height"]:
-        sprite.position.y = height - sprite_size["height"]
+    sprite.position.x = max(0, min(sprite.position.x, WIDTH - SPR_SIZE["width"]))
+    sprite.position.y = max(0, min(sprite.position.y, WIDTH - SPR_SIZE["height"]))
 
-#usage of scenechanger
-#use the name of the key as the scene name
-#to add a function to a scene use it's name without the brackest("()")
 
-#to change scene change the scene variable to the name of the scene
-scenes={"main":mainroom, "scenetest":test}
+scenes = {
+    "main": mainroom,
+    "scenetest": test
+}
 
 def main():
-    
-    global default_title, height, width#, sprite_width
+    global TITLE, HEIGHT, WIDTH
     rect.init(window)
     rect0.init(window)
     # text.init(window)
@@ -132,8 +109,8 @@ def main():
 
     while window.running:
         keys = window.frame()
-        scenes[scene](keys)
-        
+        scenes[SCENE](keys)
+
         if keys[27]:
             return 1
 
